97d6d41ecdb280a48509506aab1fb5d79841a212
Sebastian Wilgosz <sebastian@driggl.com>
2024-10-13 16:49:03 +0200

    Re-use view generator from action generator
    This way, we can only update view templates and make sure they always generate correct files

--- lib/hanami/cli_bulma/generators/app/action.rb
+++ lib/hanami/cli_bulma/generators/app/action.rb
@@ -10,6 +10,51 @@ module Hanami
         # @since 0.0.1
         # @api private
         class Action < Hanami::CLI::Generators::App::Action
+          def call(app, controller, action, url, http, format, skip_view, slice, context: nil)
+            context ||= Hanami::CLI::Generators::App::ActionContext.new(inflector, app, slice, controller, action)
+            if slice
+              generate_for_slice(controller, action, url, http, format, skip_view, slice, context)
+            else
+              generate_for_app(controller, action, url, http, format, skip_view, context)
+            end
+
+            view_directory = fs.join("app", "views", controller)
+            if generate_view?(skip_view, action, view_directory)
+              view_generator = Generators::App::View.new(fs: fs, inflector: inflector)
+              view_name = [action, controller].join(Hanami::CLI::Commands::App::Command::ACTION_SEPARATOR)
+              view_generator.call(app, view_name, format, slice)
+            end
+          end
+
+          private
+
+          # rubocop:disable Metrics/AbcSize
+          def generate_for_slice(controller, action, url, http, format, skip_view, slice, context)
+            slice_directory = fs.join("slices", slice)
+            raise Hanami::CLI::Errors::MissingSliceError.new(slice) unless fs.directory?(slice_directory)
+
+            fs.inject_line_at_block_bottom(
+              fs.join("config", "routes.rb"),
+              slice_matcher(slice),
+              route(controller, action, url, http)
+            )
+
+            fs.mkdir(directory = fs.join(slice_directory, "actions", controller))
+            fs.write(fs.join(directory, "#{action}.rb"), t("slice_action.erb", context))
+          end
+
+          def generate_for_app(controller, action, url, http, format, skip_view, context)
+            fs.inject_line_at_class_bottom(
+              fs.join("config", "routes.rb"),
+              "class Routes",
+              route(controller, action, url, http)
+            )
+
+            fs.mkdir(directory = fs.join("app", "actions", controller))
+            fs.write(fs.join(directory, "#{action}.rb"), t("action.erb", context))
+          end
+          # rubocop:enable Metrics/AbcSize
+
           def template(path, context)
             require "erb"
 
